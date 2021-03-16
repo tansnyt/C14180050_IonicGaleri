9b6af8cf462b198973a0068236259e3e9fe1ddc7
tansnyt <taniasunyoto22@gmail.com>
2021-03-16 20:20:53 +0700

    about us done

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  angka: number = 0;
+  tebak: number;
+  jawaban: boolean = false;
+  constructor() { }
 
-  constructor() {}
+  ngOnInit() {
+    this.angka = Math.floor((Math.random() * 10) + 1);
+  }
 
+  cekAngka() {
+    if (this.tebak == this.angka) {
+      this.jawaban = true;
+    } else this.jawaban = false;
+    this.tebak = null;
+  }
 }
